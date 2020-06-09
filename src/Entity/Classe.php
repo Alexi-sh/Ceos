2c04cbeab66b3671abf5174b43234ab08f19dcae
Alexi RODRIGUES <alexi.r.fernandes@gmail.com>
2020-06-09 16:08:07 +0200

    Nouvelle BDD avec relation n°2

---
+++ src/Entity/Classe.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClasseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClasseRepository::class)
+ */
+class Classe
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $niveau;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $section;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(string $niveau): self
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+
+    public function getSection(): ?string
+    {
+        return $this->section;
+    }
+
+    public function setSection(string $section): self
+    {
+        $this->section = $section;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Ressource.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Column;
+use App\Repository\RessourceRepository;
+
+/**
+ * @ORM\Entity(repositoryClass=RessourceRepository::class)
+ */
+class Ressource
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     *@Column(type="string", columnDefinition="ENUM('devoir_a_rendre', 'devoir_rendu', 'cours')"))
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="string", length=255 , nullable=true)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_limite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeInterface $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getDateLimite(): ?\DateTimeInterface
+    {
+        return $this->date_limite;
+    }
+
+    public function setDateLimite(?\DateTimeInterface $date_limite): self
+    {
+        $this->date_limite = $date_limite;
+
+        return $this;
+    }
+}
