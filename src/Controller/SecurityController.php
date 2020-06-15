47f4afff7be6e0f583cd249d4692b5524eaa5990
Mathieu-Cabare <mathieu.cabare@gmail.com>
2020-06-15 11:16:34 +0200

    login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,30 +11,30 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/loginProf", name="loginProf")
+     * @Route("/login"), name="login")
      * 
      */
-    public function loginProf(AuthenticationUtils $authenticationUtils){
-        
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/loginProf.html.twig', [
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
     }
 
-    public function redirection(){
-        
-        if($this->getUser()->hasRole('ROLE_USER'))
+    public function redirection()
+    {
+
+        if ($this->getUser()->hasRole('ROLE_USER'))
             return $this->redirect($this->generateUrl('professeur'));
-        else if($this->getUser()->hasRole('ROLE_USER_ELEVE'))
+        else if ($this->getUser()->hasRole('ROLE_USER_ELEVE'))
             return $this->redirect($this->generateUrl('eleve'));
     }
-
 }
-
