f9e92b092c4bd0dcf04497a0cae7adb9cb9c6c72
Mathieu-Cabare <mathieu.cabare@gmail.com>
2020-06-15 16:07:59 +0200

    formulaire upload fichier

--- src/Form/RessourceType.php
+++ src/Form/RessourceType.php
@@ -2,29 +2,58 @@
 
 namespace App\Form;
 
+use DateTime;
 use App\Entity\Ressource;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RessourceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('type'
-                // 'type',
-                // RessourceType::class,
-                // array(
-                //     'Devoir' => 'devoir_a_rendre',
-                //     'Cours' => 'cours'
-                // )
-
-            )
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Devoir' => 'devoir_a_rendre',
+                    'Cours' => 'cours'
+                ],
+            ])
             ->add('Titre')
             ->add('Description')
-            ->add('link')
-            ->add('date_limite');
+            // ->add('link')
+            ->add('link', FileType::class, [
+                'label' => 'Fichier à joindre',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '6000k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('date_limite', DateType::class, array(
+                'label' => 'A faire pour le',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
